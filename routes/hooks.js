b18c6f30be2c6fddfe428e7fe18a185d4c569883
Andy March <mail@andymarch.co.uk>
2019-11-14 16:28:01 +0000

    Patching in the entity id for entity users

--- routes/hooks.js
+++ routes/hooks.js
@@ -22,9 +22,23 @@ module.exports = function (){
         let responsePayload = {}
         try{
             console.log(req.body.data)
+            var parentEntity = await axios.get(process.env.TENANT+'api/v1/users/'+req.body.data.context.session.userId + 
+            "/linkedObjects/parentEntity")
+            console.log(parentEntity)
             var structure = {}
             var commands = 'commands'
             structure[commands] = []
+            var accessCommand = {
+                'type': 'com.okta.access.patch',
+                'value': [
+                    {
+                        'op': 'add',
+                        'path': '/claims/entityid',
+                        'value': parentEntity.data.profile.entityId
+                    }
+                ]
+            }
+            structure[commands].push(accessCommand)
             res.status(200).json(structure)
         } catch(error){
             console.log(error)
