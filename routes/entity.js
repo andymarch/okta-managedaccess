8d10c857b3258bbfa950cb504d8b6b2c6180f3e2
Andy March <mail@andymarch.co.uk>
2019-11-28 10:46:02 +0000

    Fixing agency reference in removal function

--- routes/entity.js
+++ routes/entity.js
@@ -47,29 +47,23 @@ router.delete("/agents", async function(req,res) {
         var resp = await axios.get(process.env.TENANT+'api/v1/users/'+req.query.id)
         console.log(resp.data)
         if(resp.data.type.id == process.env.ENTITY_TYPE_ID){
-            var agentQuery = await axios.get(process.env.TENANT+'api/v1/users/'+req.query.agentid)
-            if(agentQuery.data.type.id == process.env.AGENCY_TYPE_ID){
-                var agents = resp.data.profile.delegatedAgents
-                console.log(agents)
-                var filtered = agents.filter(function(value, index, arr){
-                    console.log(value + " "+ req.query.agentid)
-                    console.log(JSON.stringify(value) !== req.query.agentid)
-                    return value !== req.query.agentid;
-                });
+            var agents = resp.data.profile.delegatedAgents
+            console.log(agents)
+            var filtered = agents.filter(function(value, index, arr){
+                console.log(value + " "+ req.query.agentid)
+                console.log(JSON.stringify(value) !== req.query.agentid)
+                return value !== req.query.agentid;
+            });
 
-                console.log(filtered)
+            console.log(filtered)
 
-                var payload = {
-                    profile: {
-                        delegatedAgents: filtered
-                    }
+            var payload = {
+                profile: {
+                    delegatedAgents: filtered
                 }
-                await axios.post(process.env.TENANT+'api/v1/users/'+req.query.id,payload)
-                res.status(200).send();
             }
-            else{
-                res.status(200).json({error: "Not an agency"})
-            }
+            await axios.post(process.env.TENANT+'api/v1/users/'+req.query.id,payload)
+            res.status(200).send();
         }
         else {
             res.status(200).json({error: "Not an entity"})
